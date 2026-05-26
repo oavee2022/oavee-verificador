c1924a2ee3f221774a8165feb1e88c8f3163cee6
oavee2022 <oavee.2022@gmail.com>
2026-05-26 12:25:52 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBtYHq6tRHXJwTLOmxIaMBIFG-NXAK2IS4",
+  authDomain: "oavee-validador.firebaseapp.com",
+  databaseURL: "https://oavee-validador-default-rtdb.firebaseio.com",
+  projectId: "oavee-validador",
+  storageBucket: "oavee-validador.firebasestorage.app",
+  messagingSenderId: "720264341114",
+  appId: "1:720264341114:web:4e1b64fc60819715c97dc3"
+};
